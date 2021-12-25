dbd96f0071978a952d9782eef20effe8d4f0b40e
marufmarzuq <marufmarzuq18@gmail.com>
2021-12-25 17:07:32 +0600

    menu bar added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,41 +1,52 @@
 import "./Header.css";
+import { useState } from "react";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import CloseIcon from "@mui/icons-material/Close";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import MenuIcon from "@mui/icons-material/Menu";
 import Badge from "@mui/material/Badge";
+import HeaderMenu from "../headerMenu/HeaderMenu";
 
 const Header = () => {
+    const [menuStatus, setMenuStatus] = useState(false);
+
     return (
         <header>
             <div className="container">
                 <div className="toggle">
-                    <span>
-                        <MenuIcon fontSize="medium" />
+                    <span onClick={() => setMenuStatus(!menuStatus)}>
+                        {!menuStatus ? <MenuIcon fontSize="medium" /> : <CloseIcon fontSize="medium" />}
                     </span>
                 </div>
-                <div className="logo">
-                    <a href="/">iStore</a>
-                </div>
-                <nav>
-                    <div className="nav-item">
-                        <span>
-                            <FavoriteBorderOutlinedIcon />
-                        </span>
-                    </div>
-                    <div className="nav-item">
-                        <span>
-                            <Badge badgeContent={4} color="primary">
-                                <ShoppingCartOutlinedIcon />
-                            </Badge>
-                        </span>
-                    </div>
-                    <div className="nav-item">
-                        <span>
-                            <AccountCircleOutlinedIcon />
-                        </span>
-                    </div>
-                </nav>
+                {menuStatus ? (
+                    <HeaderMenu />
+                ) : (
+                    <>
+                        <div className="logo">
+                            <a href="/">iStore</a>
+                        </div>
+                        <nav>
+                            <div className="nav-item">
+                                <span>
+                                    <FavoriteBorderOutlinedIcon />
+                                </span>
+                            </div>
+                            <div className="nav-item">
+                                <span>
+                                    <Badge badgeContent={4} color="primary">
+                                        <ShoppingCartOutlinedIcon />
+                                    </Badge>
+                                </span>
+                            </div>
+                            <div className="nav-item">
+                                <span>
+                                    <AccountCircleOutlinedIcon />
+                                </span>
+                            </div>
+                        </nav>
+                    </>
+                )}
             </div>
         </header>
     );
---
+++ src/components/headerMenu/HeaderMenu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HeaderMenu.css";
+
+const HeaderMenu = () => {
+    return (
+        <div className="header-menu">
+            <a href="#">Shop</a>
+            <a href="#">Blog</a>
+            <a href="#">Contact</a>
+        </div>
+    );
+};
+
+export default HeaderMenu;
